3f3771f52f43bbf5b912068ca1a6582cb6343601
Arjan Molenaar <gaphor@gmail.com>
2009-09-17 07:51:12 +0200

    Added tests for build in and library types.

---
+++ tests/test_types.py
@@ -0,0 +1,89 @@
+
+
+import unittest
+from roles import RoleType, clone
+
+
+class SimpleRole(object):
+    __metaclass__ = RoleType
+    def inrole(self):
+        return "in role"
+
+
+class TypesTest(unittest.TestCase):
+    """
+    Test application of roles on types.
+    """
+
+    def test_dict(self):
+        d = dict()
+        try:
+            SimpleRole(d)
+        except TypeError, e:
+            self.assertEquals("__class__ assignment: only for heap types", str(e))
+        else:
+            assert false, "should not be reached"
+
+    def test_list(self):
+        d = ['a', 'b']
+        try:
+            SimpleRole(d)
+        except TypeError, e:
+            self.assertEquals("__class__ assignment: only for heap types", str(e))
+        else:
+            assert false, "should not be reached"
+
+
+    def test_tuple(self):
+        d = ('a', 'b')
+        try:
+            SimpleRole(d)
+        except TypeError, e:
+            self.assertEquals("__class__ assignment: only for heap types", str(e))
+        else:
+            assert false, "should not be reached"
+
+
+
+    def test_userdict(self):
+        from UserDict import UserDict
+        d = UserDict()
+        try:
+            SimpleRole(d)
+        except TypeError, e:
+            self.assertEquals("type 'instance' is not an acceptable base type", str(e))
+        else:
+            assert false, "should not be reached"
+
+
+    def test_namedtuple(self):
+        """
+        Can't assign roles to namedtuple's.
+        """
+        from collections import namedtuple
+        import math
+
+        Point = namedtuple('Point', 'x y')
+        p = Point(1,2)
+
+        class Vector:
+            __metaclass__ = RoleType
+            def m(self):
+                "Manhattan style distance calculation"
+                return p.x + p.y
+            def distance(self):
+                return math.sqrt(p.x * p.x + p.y * p.y)
+
+        try:
+            Vector(p)
+        except TypeError, e:
+            self.assertEquals("__class__ assignment: 'Point' object layout differs from 'Point+Vector'", str(e))
+        else:
+            assert false, "should not be reached"
+        #assert p.manhattan() == 3
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vi: sw=4:et:ai
