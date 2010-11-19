fea2d817e40d5bf812536e2059ab0c73115b9ca6
Arjan Molenaar <gaphor@gmail.com>
2010-11-19 14:21:58 +0100

    Added Django example module

---
+++ django_dci/account/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Account(models.Model):
+
+    balance = models.DecimalField(decimal_places=2, max_digits=12)
+    
+    def withdraw(self, amount):
+        print "Withdraw " + str(amount) + " from " + str(self)
+        self.balance -= amount
+
+    def deposit(self, amount):
+        print "Deposit " + str(amount) + " in " + str(self)
+        self.balance += amount
+
+
+
+# vim:sw=4:et:ai
