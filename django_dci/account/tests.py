cc36baff1333f4cdab34eb712f50a8fca75f4ce4
Arjan Molenaar <gaphor@gmail.com>
2010-11-19 14:52:17 +0100

    Implemented account example in Django (test case).

--- django_dci/account/tests.py
+++ django_dci/account/tests.py
@@ -6,18 +6,83 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Account
 
-class SimpleTest(TestCase):
+from roles import RoleType
+from roles.context import context
+from contextlib import nested
+
+from django.db.models.base import ModelBase
+
+class DjangoRoleType(RoleType, ModelBase):
+    pass
+
+
+class MoneySource(object):
+    __metaclass__ = DjangoRoleType
+
+    def transfer(self, amount):
+        if self.balance >= amount:
+            self.withdraw(amount)
+            context.sink.receive(amount)
+
+
+class MoneySink(object):
+    __metaclass__ = DjangoRoleType
+
+    def receive(self, amount):
+        self.deposit(amount)
+
+
+class TransferMoney(object):
+
+    def __init__(self, source, sink):
+        self.source = source
+        self.sink = sink
+
+    def transfer_money(self, amount):
+        """
+        The interaction.
+        """
+        with nested(context(self),
+                    MoneySource.played_by(self.source),
+                    MoneySink.played_by(self.sink)):
+            self.source.transfer(amount)
+            print "We can still access the original attributes", self.sink.balance
+            print "Is it still an Account?", isinstance(self.sink, Account)
+            #print "Object equality?", dst == self.sink
+        self.source.save()
+        self.sink.save()
+
+
+class MoneyTransferTest(TestCase):
+
     def test_basic_addition(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test roles on Django model classes.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        src = Account(balance=1000)
+        dst = Account(balance=0)
+        src.save()
+        dst.save()
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        accounts = Account.objects.all()
+        assert len(accounts) == 2
+        assert accounts[0].balance == 1000, accounts[0].balance
+        assert accounts[1].balance == 0, accounts[1].balance
+        ctx = TransferMoney(src, dst)
 
->>> 1 + 1 == 2
-True
-"""}
+        ctx.transfer_money(amount=100)
 
+        print src, src.balance
+        assert src.balance == 900
+        print dst, dst.balance
+        assert dst.balance == 100
+
+        accounts = Account.objects.all()
+        assert accounts[0].balance == 900, accounts[0].balance
+        assert accounts[1].balance == 100, accounts[1].balance
+
+
+
+# vim:sw=4:et:ai
