24db32551afb10f8d41d24e701059f88cabfa0d2
Arjan Molenaar <gaphor@gmail.com>
2010-11-22 16:05:21 +0100

    Django module cleanup.

--- roles/django.py
+++ roles/django.py
@@ -4,13 +4,13 @@ Support for Django.
 
 from __future__ import absolute_import
 
-from .role import RoleType, class_fields, EXCLUDED
 try:
     from django.db.models.base import ModelBase
 except ImportError, e:
     import logging
     logging.warning('Django could not be imported: %s' % str(e))
 else:
+    from .role import RoleType, class_fields
 
     overrides = class_fields(RoleType).intersection(class_fields(ModelBase))
     assert not overrides, 'Methods in RoleType should not override methods in ModelBase (%s)' % (overrides,)
